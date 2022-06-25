b7d4b4129cf0b9cd337a0b89b98c712637e034d7
Pawel <wilkowski@gmail.com>
2022-06-25 05:12:54 +0200

    initial

---
+++ GcodeSlower/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GcodeSlower {
+	internal static class Program {
+		static private void Main(string[] args) {
+			if (args.Length < 1) {
+				Console.WriteLine("No args provided!");
+				return;
+			}
+
+			var gcodeFileName = args[0];
+
+			var lines = new List<string>(File.ReadAllLines(gcodeFileName));
+			var startLineIndexes = new List<int>();
+
+			for (var i = 0; i < lines.Count; i++) {
+				var line = lines[i];
+				if (line.StartsWith(";LAYER_CHANGE")) {
+					startLineIndexes.Add(i);
+				}
+			}
+
+			var lastFullSpeedLayer = 5;
+			var minSpeed = 25;
+			for (var i = lastFullSpeedLayer; i >= 0; i--) {
+				var speed = (
+					minSpeed + (i * (100 - minSpeed) / lastFullSpeedLayer)
+				).ToString("D");
+
+				var lineIdx = startLineIndexes[i];
+				Console.WriteLine($"M220 S{speed} at line {lineIdx}");
+				lines.Insert(lineIdx, $"M220 S{speed}");
+			}
+
+			Console.WriteLine("Layers " + startLineIndexes.Count);
+			File.WriteAllLines(gcodeFileName + "p.gcode", lines);
+		}
+	}
+}
